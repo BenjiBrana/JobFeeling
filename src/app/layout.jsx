2ce1a8befe9c43fc525f5c1d672eef2d3d9a68b9
Benji Brana <benjidebran@gmail.com>
2024-04-18 16:59:41 +0200

    ajout meta description+name

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+import './globals.css';
+import React from 'react';
+
+export const metadata = {
+  title: 'JobFeeling',
+  description:
+    'Découvrez JobFeeling, votre plateforme de recrutement en ligne de confiance. Connectez-vous avec les meilleurs talents et trouvez l’emploi de vos rêves. Inscrivez-vous dès maintenant !',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
