a97f195b339fb661f7bb8f3aaffacbf052f279cc
Benji Brana <benjidebran@gmail.com>
2024-04-30 16:33:27 +0200

    modification middleware

--- src/app/Middleware/CacheHeaders.jsx
+++ src/app/Middleware/CacheHeaders.jsx
@@ -1,14 +1,15 @@
-export default function cacheHeaders(res, next) {
+export default function cacheHeaders(req, res, next) {
   /* Ajouter les en-têtes de sécurité */
-
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
 
-  /* Ajouter l'en-tête Cache-Control existant  */
-  res.setHeader(
-    'Cache-Control',
-    'public, max-age=31536000, must-revalidate'
-  );
+  /* Ajouter l'en-tête Cache-Control existant si aucun en-tête n'est défini */
+  if (!res.getHeader('Cache-Control')) {
+    res.setHeader(
+      'Cache-Control',
+      'public, max-age=31536000, must-revalidate'
+    );
+  }
 
   /* Appeler next() pour passer au middleware suivant*/
   next();
