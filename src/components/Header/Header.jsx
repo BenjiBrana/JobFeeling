b2456f7c2731d1fdeb099ba8418d6865e59dbac5
Benji Brana <benjidebran@gmail.com>
2024-05-02 10:43:41 +0200

    correctif mineur

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -159,6 +159,7 @@ export default function Header() {
               id="menuIcon"
               className=" h-10 w-10 z-10 bg-background/70  hover:bg-tertinary/70 dark:hover:bg-tertinaryDark/70 rounded-full p-1 shadow-md shadow-black dark:shadow-white"
               xmlns="http://www.w3.org/2000/svg"
+              role="img"
               viewBox="0 0 448 512"
             >
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
